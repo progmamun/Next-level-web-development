491d66958588aef0be6594e50466ee283d976b83
progmamun <mamun333089@gmail.com>
2023-05-14 06:47:40 +0600

    interface-and-schema

--- Mission-02/Module-06/src/app.ts
+++ Mission-02/Module-06/src/app.ts
@@ -1,14 +1,49 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response, urlencoded } from "express";
 import cors from "cors";
+import { Schema } from "mongoose";
 
 const app: Application = express();
 
+// using cors 
 app.use(cors());
 
+// parse data
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Hello World!');
-    next();
+    // insert a test data 
+    // create an interface 
+    interface IUser {
+      id: string;
+      role: "student";
+      password: string;
+      name: {
+        firstName: string;
+        middleName?: string;
+        lastName: string;
+      };
+      dateOfBirth?: number;
+      gender: "male"|"female";
+    }
+    // creating schema using interface 
+    const userSchema = new Schema <IUser> ({
+      id: {type: String, required: true, unique: true},
+      role: {type: String, required: true},
+      password: {type: String, required: true},
+      name: {
+        firstName: {type: String, required: true},
+        middleName: {type: String,},
+        lastName: {type: String, required: true},
+      },
+      dateOfBirth: {type: Number},
+      gender: {type: String, enum: ["male", "female"]}
+    });
+    
   });
+  
+
+
   
 
 export default app;
--- Mission-02/Module-06/src/server.ts
+++ Mission-02/Module-06/src/server.ts
@@ -3,18 +3,18 @@ import app from "./app";
 
 const port: number = 5000;
 
+
 // database connection 
 async function bootstrap() {
     try {
         await mongoose.connect("mongodb://127.0.0.1:27017/practice-mongoose");
         console.log(`database connection successful.`);
+        
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}`)
+        });
     } catch (err) {
         console.log(`Fail to connect Database: error code 500`, err);
     }
 }
 bootstrap();
-
-
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`)
-});
